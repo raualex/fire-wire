f0fd4436bb2b80164b382c7e91df756c882e92f5
Alexander Rau <alexanderrau86@yahoo.com>
2018-10-20 13:49:07 -0600

    Start Info page build

---
+++ src/Information.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Information.css';
+
+class Information extends Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return (
+      <div className='fire-information-container' >
+        <h1>Information</h1>
+      </div>
+    )
+  }
+}
+
+export default Information;
