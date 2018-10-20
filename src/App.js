f8c5df7276c81411df831b29815eaca35e421fc6
Cierra Washington <cierrajwashi@gmail.com>
2018-10-20 14:34:46 -0600

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import fireImg from './fire.png';
+import cameraImg from './photo-camera.png'
+import menu from './menu.png'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main-div">
+      <header className="header-menu">
+        <img src={menu} className="menu-img"/>
+      </header>
+        <section className="home-container">
+        <h2 className="fire-title">FIRE<span className="fire-dash">-</span>WIRE</h2>
+        <img src={fireImg} className='fire-img'/>
+        <h2 className="report-fire">Report a Fire</h2>
+        <img src={cameraImg} className='camera-img'/>
+        </section>
       </div>
     );
   }
