082ecd7cc3295ea1c48402b73520b930789b79b7
Cierra Washington <cierrajwashi@gmail.com>
2018-10-20 15:29:25 -0600

    add react router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import fireImg from './fire.png';
 import cameraImg from './photo-camera.png'
 import menu from './menu-1.png'
+import { Link } from 'react-router-dom';
+import { Route, NavLink} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -16,7 +18,7 @@ class App extends Component {
         <h2 className="fire-title">FIRE<span className="fire-dash">-</span>WIRE</h2>
         <img src={fireImg} className='fire-img'/>
         <h2 className="report-fire">Report a Fire</h2>
-        <img src={cameraImg} className='camera-img'/>
+        <NavLink to='/information' className='nav'><img src={cameraImg} className='camera-img'/></NavLink>
         </section>
       </div>
     );
